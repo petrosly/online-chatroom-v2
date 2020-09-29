4be8bfa3df1fbae450bc6890ce9f4d6429b43db2
petrosly <petroslymper@gmail.com>
2020-09-30 01:03:35 +0300

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user');
-const config = require ('../config/database');
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const app = express();
